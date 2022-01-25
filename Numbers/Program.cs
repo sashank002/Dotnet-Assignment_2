1aac69ea11514d43bb8e9e94c05bef2fb00de27a
sashank parmar <parmarsatvan@gmail.com>
2022-01-25 13:27:07 +0530

    final

---
+++ Numbers/Program.cs
@@ -0,0 +1,112 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace Program 
+{
+    class MainClass 
+    {
+        public static void Main(string[] args)
+        {
+            NumberExample.IntExample();
+            NumberExample.DoubleExample();
+            NumberExample.DecimalExample();
+            NumberExample.FormulaExample();
+        }
+    }
+
+    static class NumberExample
+    {
+        public static void IntExample()
+        {
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine("hello integer");
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine("Math Operations : : :");
+            Console.WriteLine("a+b = " + c);
+            c = a-b;
+            Console.WriteLine("a-b = " + c);
+            c = a*b;
+            Console.WriteLine("a*b = " + c);
+            c = a/b;
+            Console.WriteLine("a/b = " + c);
+
+            Console.WriteLine("ORDER OF OPERATIONS : :");
+            a = 5;
+            b = 4;
+            c = 2;
+            int d = a+b*c;
+            Console.WriteLine("a + b * c = " + d);
+            Console.WriteLine("(a + b) * c = " + (a+b)*c);
+            Console.WriteLine("(a + b) - 6 * c + (12 * 4) / 3 + 12 = " + ((a + b) - 6 * c + (12 * 4) / 3 + 12));
+            Console.WriteLine("(a + b) / c = " + (a+b)/c);
+
+            Console.WriteLine("INTEGER PRECISION & LIMITS : :");
+            a = 7;
+            b = 4;
+            c = 3;
+            d = (a+b) / c;
+            int e = (a+b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is (min : ) {min} to (max : ) {max}");
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow (max + 3) : {what}");
+
+
+        }
+
+        public static void DoubleExample() 
+        {
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine("Double Type Example : :");
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a + b) / c;
+            Console.WriteLine($"(a + b) / c {d}");
+
+            Console.WriteLine("MORE COMPLICATED EXAMPLE OF DOUBLE : :");
+            a = 19;
+            b = 23;
+            c = 8;
+            d = (a+b)/c;
+            Console.WriteLine($"(a + b) / c {d}");
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine($"Rounding error : {third}");
+
+        }
+
+        public static void DecimalExample()
+        {
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine("DECIMAL EXAMPLE : : ");
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine( "IN DECIMAL (c / d) = " + (c / d));
+        }
+
+        public static void FormulaExample()
+        {
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine("FINDING AREA USING Math.PI : : ");
+            double radius = 2.50;
+            double area = Math.PI * radius * radius;
+            Console.WriteLine("AREA : " + area);
+        }
+    }
+}
---
+++ branch & loop/Program.cs
@@ -0,0 +1,121 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System;
+
+namespace Program 
+{
+    class MainClass 
+    {
+        public static void Main(string[] args)
+        {
+        //    BranchLoopClass.IfExample();
+            // BranchLoopClass.LoopExample();
+            BranchLoopClass.FinalExample();
+        }
+    }
+
+    static class BranchLoopClass
+    {
+        public static void IfExample()
+        {
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine(" IF STATEMENT EXAMPLE  : : ");
+            int a = 5;
+            int b = 6;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10.");
+
+
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine(" IF-ELSE STATEMENT EXAMPLE  : : ");
+             a = 5;
+             b = 3;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10");
+            else
+                Console.WriteLine("The answer is not greater than 10");
+
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine(" IF-ELSE STATEMENT COMPLICATED EXAMPLE  : : ");
+            a = 5;
+            b = 3;
+            int c = 4;
+            if ((a + b + c > 10) && (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+            }
+
+        }
+
+        public static void LoopExample() 
+        {
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine(" WHILE - LOOP EXAMPLE  : : ");
+
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
+
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine(" DO-WHILE - LOOP EXAMPLE  : : ");
+            counter = 0;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+
+
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine(" FOR - LOOP EXAMPLE  : : ");
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Hello World! The counter is {i}");
+            }
+
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine(" NESTED - FOR - LOOP EXAMPLE  : : ");
+
+            Console.WriteLine("ROW,COLUMN EXAMPLE : ");
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column})");
+                }
+            }
+        }
+
+        public static void FinalExample()
+        {
+            Console.WriteLine("__________________________________________");
+            Console.WriteLine(" FINAL EXAMPLE  : : ");
+
+            int sum = 0;
+            for (int number = 1; number < 21; number++)
+            {
+                if (number % 3 == 0)
+                {
+                    sum = sum + number;
+                }
+            }
+            Console.WriteLine($"The sum is {sum}");
+
+        }
+    }
+
+ 
+}
+
